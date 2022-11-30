711e82c9a9c367ae3a3b80b5c3f52c5ab6fb3d12
Diego9765 <diego.bautista7324@utc.edu.ec>
2022-11-30 06:40:30 -0500

    Update footer.php
    AUTORES

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,7 +2,7 @@
 
 <br><br><br><br><br><br><br><br><br><br><br><br><br>
 <div class="well text-center">
-  <h4><b><i class="glyphicon glyphicon-user"></i>REALIZADO POR:</b></h4>
+  <h4><b><i class="glyphicon glyphicon-user"></i>REALIZADO POR:JAVIER LASSO</b></h4>
 </div>
   </body>
 </html>
